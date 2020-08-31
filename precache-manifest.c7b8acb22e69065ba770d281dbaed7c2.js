dd0c5b31802b389dace8c9b3181406c022e2015d
Kartik Bhushan <kartik@xecurify.com>
2020-08-31 09:14:02 +0530

    Updates

---
+++ precache-manifest.c7b8acb22e69065ba770d281dbaed7c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5018f400e5277af9ba76a1ffb4e6c057",
+    "url": "/Task-manager/index.html"
+  },
+  {
+    "revision": "fdc93874a09b695aad35",
+    "url": "/Task-manager/static/css/main.7bb83924.chunk.css"
+  },
+  {
+    "revision": "90dc000eedd55178d0e6",
+    "url": "/Task-manager/static/js/2.da6174fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-manager/static/js/2.da6174fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc93874a09b695aad35",
+    "url": "/Task-manager/static/js/main.525dc7d1.chunk.js"
+  },
+  {
+    "revision": "409ae3ad5ed5f2546e01",
+    "url": "/Task-manager/static/js/runtime-main.a9994eb3.js"
+  }
+]);
